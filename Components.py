46cec1e888d27d8719085bf6672d148967773281
AnayG <anayg.gokhale@gmail.com>
2025-11-02 22:26:27 -0600

    fixed names issue; Now actual names are printed

--- Components.py
+++ Components.py
@@ -2,25 +2,26 @@
 Included components: Wire, Battery, Resistor, LED
 '''
 class Wire:
-    def __init__(self, node1, node2):
+    def __init__(self, node1, node2, name="Wire"):
         self.node1 = node1
         self.node2 = node2
-
+        self.name = name
 class Battery:
-    def __init__(self, node1, node2, voltage):
+    def __init__(self, node1, node2, voltage, name="Battery"):
         self.node1 = node1
         self.node2 = node2
         self.voltage = voltage
-
+        self.name = name
 class Resistor:
-    def __init__(self, node1, node2, resistance):
+    def __init__(self, node1, node2, resistance, name="Resistor"):
         self.node1 = node1
         self.node2 = node2
         self.resistance = resistance
-
+        self.name = name
 class LED:
-    def __init__(self, node1, node2, resistance, color):
+    def __init__(self, node1, node2, resistance, color, name="LED"):
         self.node1 = node1
         self.node2 = node2
         self.resistance = resistance
         self.color = color
+        self.name = name
