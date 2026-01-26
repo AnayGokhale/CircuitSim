a8293607c6441e8d2eab174933ec950955eb11f8
AnayG <anayg.gokhale@gmail.com>
2026-01-25 19:36:03 -0600

    updating main branch - latest progress is disjoint set logic

--- Components.py
+++ Components.py
@@ -7,21 +7,27 @@ class Wire:
         self.node2 = node2
         self.name = name
 class Battery:
-    def __init__(self, node1, node2, voltage, name="Battery"):
+    def __init__(self, node1, node2, node_id_1, node_id_2, voltage, name="Battery"):
         self.node1 = node1
         self.node2 = node2
+        self.node_id_1 = node_id_1
+        self.node_id_2 = node_id_2
         self.voltage = voltage
         self.name = name
 class Resistor:
-    def __init__(self, node1, node2, resistance, name="Resistor"):
+    def __init__(self, node1, node2, node_id_1, node_id_2, resistance, name="Resistor"):
         self.node1 = node1
         self.node2 = node2
+        self.node_id_1 = node_id_1
+        self.node_id_2 = node_id_2
         self.resistance = resistance
         self.name = name
 class LED:
-    def __init__(self, node1, node2, resistance, color, name="LED"):
+    def __init__(self, node1, node2, node_id_1, node_id_2, resistance, color, name="LED"):
         self.node1 = node1
         self.node2 = node2
+        self.node_id_1 = node_id_1
+        self.node_id_2 = node_id_2
         self.resistance = resistance
         self.color = color
         self.name = name
--- Physics.py
+++ Physics.py
@@ -1,28 +1,9 @@
 from Components import Wire, Battery, Resistor, LED
+
+
+
+#def generate_incidence_matrix(components):
     
-def path_matrix(components):
-    incidence_matrix = []
-    for n in range(len(components)):
-        incidence_matrix.append([0] * len(components))
-    start_nodes = [component.node1 for component in components]
-    end_nodes = [component.node2 for component in components]
-    for endNode in end_nodes:
-        for startNode in start_nodes:
-            if endNode == startNode:
-                raise ValueError("Cannot create a path between the same node")
-            if endNode is None or startNode is None:
-                raise ValueError("Node cannot be None")
-            if endNode[0] == startNode[0]:
-                c1 = end_nodes.index(endNode)
-                c2 = start_nodes.index(startNode)
-                incidence_matrix[c1][c1] = -1
-                incidence_matrix[c1][c2] = 1
-            elif endNode[1] == startNode[1]:
-                c1 = end_nodes.index(endNode)
-                c2 = start_nodes.index(startNode)
-                incidence_matrix[c1][c1] = -1
-                incidence_matrix[c1][c2] = 1
-    return incidence_matrix
 
 def calculate_resistance_in_series(resistances):
     return sum(resistances)
